3c5ab9b00923d3fbae5d02f1d02620d3adf2049a
Tasnim Jahan Urmi <2230343@iub.edu.bd>
2026-04-26 13:01:51 +0600

    Add port and SSL support for Aiven integration

--- api/config/database.php
+++ api/config/database.php
@@ -4,6 +4,7 @@ class Database {
     private $db_name;
     private $username;
     private $password;
+    private $port;
     public $conn;
 
     public function __construct() {
@@ -11,15 +12,17 @@ class Database {
         $this->db_name = getenv('DB_NAME') ?: 'urban_farming';
         $this->username = getenv('DB_USERNAME') ?: 'root';
         $this->password = getenv('DB_PASSWORD') ?: '';
+        $this->port = getenv('DB_PORT') ?: '3306';
     }
 
     public function getConnection() {
         $this->conn = null;
         try {
             $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                "mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name,
                 $this->username,
-                $this->password
+                $this->password,
+                array(PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt') // Add SSL support for Vercel/Aiven
             );
             $this->conn->exec("set names utf8");
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
